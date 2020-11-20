461a4aee7d00fdfbda6dbc9f8baa814c261d5221
Sathish <sathish.2009btech@gmail.com>
2020-11-20 14:14:50 +0530

    camera intermediate commit

--- app/src/main/java/com/daypay_technologies/LandingPageActivity.java
+++ app/src/main/java/com/daypay_technologies/LandingPageActivity.java
@@ -1,15 +1,33 @@
 package com.daypay_technologies;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.daypay_technologies.fragments.CameraFragment;
 
-public class LandingPageActivity extends BaseAppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class LandingPageActivity extends BaseAppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private static final int CAMERA_FRAGMENT = 1;
 
@@ -23,6 +41,21 @@ public class LandingPageActivity extends BaseAppCompatActivity implements Bottom
         setContentView(R.layout.ac_landing_page);
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        attachToolbar();
+    }
+
+    private void attachToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // get the reference of Toolbar
+        setSupportActionBar(toolbar); // Setting/replace toolbar as the ActionBar
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.my_statusbar_color));
+        }
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
     }
 
     public void displayView(int fragmentNo, String aTitle, boolean addToBackstack) {
--- app/src/main/java/com/daypay_technologies/fragments/CameraFragment.java
+++ app/src/main/java/com/daypay_technologies/fragments/CameraFragment.java
@@ -1,28 +1,44 @@
 package com.daypay_technologies.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.daypay_technologies.LandingPageActivity;
 import com.daypay_technologies.R;
 import com.daypay_technologies.widgets.CameraView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class CameraFragment extends BaseFragment {
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-    private CameraView cameraView;
+public class CameraFragment extends BaseFragment implements SurfaceHolder.Callback  {
+
+    private SurfaceHolder surfaceHolder;
     private Camera camera;
+    private SurfaceView surfaceView;
+    public static final int REQUEST_CODE = 100;
+    private String[] neededPermissions = new String[]{CAMERA, WRITE_EXTERNAL_STORAGE};
 
     public static CameraFragment newInstance() {
         CameraFragment cameraFragment = new CameraFragment();
@@ -33,41 +49,134 @@ public class CameraFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fr_camera,container,false);
-        cameraView = new CameraView(getActivity(), (SurfaceView) view.findViewById(R.id.surfaceView));
-        cameraView.setLayoutParams(new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT));
-        ((FrameLayout) view.findViewById(R.id.layout)).addView(cameraView);
-        cameraView.setKeepScreenOn(true);
+        surfaceView = view.findViewById(R.id.surfaceView);
+        if (surfaceView != null) {
+            boolean result = checkPermission();
+            if (result) {
+                setupSurfaceHolder();
+            }
+        }
         return view;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        int numCams = Camera.getNumberOfCameras();
-        if(numCams > 0) {
-            try {
-                camera = Camera.open(0);
-                camera.startPreview();
-                cameraView.setCamera(camera);
-            } catch (RuntimeException ex) {
-                Toast.makeText(getActivity(), getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
+    private boolean checkPermission() {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            ArrayList<String> permissionsNotGranted = new ArrayList<>();
+            for (String permission : neededPermissions) {
+                if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                    permissionsNotGranted.add(permission);
+                }
+            }
+            if (permissionsNotGranted.size() > 0) {
+                boolean shouldShowAlert = false;
+                for (String permission : permissionsNotGranted) {
+                    shouldShowAlert = ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permission);
+                }
+                if (shouldShowAlert) {
+                    showPermissionAlert(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]));
+                } else {
+                    requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]));
+                }
+                return false;
             }
         }
+        return true;
+    }
+
+    private void showPermissionAlert(final String[] permissions) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle(R.string.permission_required);
+        alertBuilder.setMessage(R.string.permission_message);
+        alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                requestPermissions(permissions);
+            }
+        });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    private void requestPermissions(String[] permissions) {
+        ActivityCompat.requestPermissions(getActivity(), permissions, REQUEST_CODE);
     }
 
     @Override
-    public void onPause() {
-        if(camera != null) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                for (int result : grantResults) {
+                    if (result == PackageManager.PERMISSION_DENIED) {
+                        // Not all permissions granted. Show message to the user.
+                        return;
+                    }
+                }
+
+                // All permissions are granted. So, do the appropriate work now.
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        startCamera();
+    }
+
+    private void startCamera() {
+        camera = Camera.open();
+        camera.setDisplayOrientation(90);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setupSurfaceHolder() {
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        resetCamera();
+    }
+
+    public void resetCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            // Return if preview surface does not exist
+            return;
+        }
+
+        if (camera != null) {
+            // Stop if preview surface is already running.
             camera.stopPreview();
-            cameraView.setCamera(null);
+            try {
+                // Set preview display
+                camera.setPreviewDisplay(surfaceHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Start the camera preview...
+            camera.startPreview();
+        }
+    }
+
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.stopPreview();
             camera.release();
             camera = null;
         }
-        super.onPause();
     }
 
-    private void resetCam() {
-        camera.startPreview();
-        cameraView.setCamera(camera);
-    }
 }
